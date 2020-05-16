113be673f3bdf0a91d518f6a43c8293e96814ad6
Shawn Liu <shawnliu30@gmail.com>
2020-05-16 17:11:09 -0400

    got links to company 8-ks and updated extraction functions

--- src/data/data_clean.py
+++ src/data/data_clean.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 # importing
 import numpy as np
 import pandas as pd
 
-
-# In[ ]:
-
-
 # cleaning the price data
 
 def clean_close(sp_df):
-    
     # adding normalized percentage changes
-    
+
     sp_df['range'] = sp_df['High'] - sp_df['Low']
     sp_df['close_norm'] = sp_df['Close'].pct_change()
     sp_df['low_norm'] = sp_df['Low'].pct_change()
@@ -25,11 +17,12 @@ def clean_close(sp_df):
     sp_df['open_norm'] = sp_df['Open'].pct_change()
     sp_df['vol_norm'] = sp_df['Volume'].pct_change()
     sp_df['range_norm'] = sp_df['range'].pct.change()
-    
-    # dropping unecessary features
-    
-    sp_df.drop(columns=['Date', 'Stock Splits', 'Dividends', 'Close',
-                    'Open','Low', 'High', 'Volume', 'range'], inplace=True)
-    
-    return(sp_df)
 
+    # dropping unnecessary features
+
+    sp_df.drop(columns=['Date', 'Stock Splits', 'Dividends', 'Close',
+                        'Open', 'Low', 'High', 'Volume', 'range'], inplace=True)
+
+    return sp_df
+
+# parsing text from 8-ks
--- src/data/data_extract.py
+++ src/data/data_extract.py
@@ -1,38 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[80]:
-
-
 # importing
 import numpy as np
 import pandas as pd
 
 import yfinance as yf
 import pandas_datareader as pdr
-from TwitterAPI import TwitterAPI, TwitterPager
 import csv
-
 import datetime
 
-
-# In[69]:
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
 
 
 # getting s&p price data from yahoo finance
 
 def get_yahoo_data():
-    
     # overriding with pandas datareader
-    
+
     yf.pdr_override()
-    
+
     # finding s&p index ticker and getting 5 years of data
-    
+
     sp_df = pdr.get_data_yahoo('^GSPC', start='2015-05-01', end='2020-04-30').reset_index()
 
     # splitting between month, day, and weekday
-    
+
     months = []
     days = []
     weekdays = []
@@ -45,64 +40,66 @@ def get_yahoo_data():
     sp_df['Month'] = months
     sp_df['Day_month'] = days
     sp_df['Day_week'] = weekdays
-    
-    return(sp_df)
+
+    return sp_df
 
 
-# In[70]:
+def get_edgar_data():
+    # getting CIK for every company in the s&p 500 from wikipedia
 
+    wiki_url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    cik_df = pd.read_html(wiki_url, header=0, index_col=0)[0]
+    cik_list = list(cik_df['CIK'])
 
-# twitter API authentication
+    # creating empty dataframe to append all other dataframes with 8-k links
+    doc_df = pd.DataFrame()
 
-def get_twitter_authentication():
-    
-    # variables that contain user credentials to access twitter api
-    
-    ACCESS_TOKEN = '1255891281752010754-VrK3kU7JvarLjVMsJets7QTkinwuZx'
-    ACCESS_SECRET = 'dVeuWeSpdFe91lngima25xQhlP5ijiDC2iYiP0mNKU2cC'
-    CONSUMER_KEY = 'UUCzKktOILbxhagMxyyCyjF7E'
-    CONSUMER_SECRET = 'LXYmgtey0ZoB4oayVEWvgjZ0XUxmwSVuR2bNW9fXWVvZC5klVs'
+    for cik in cik_list:
 
-    api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
-    
-    return(api)
+        try:
 
+            # defining endpoint and parameters for every company in the s&p
+            url = 'https://www.sec.gov/cgi-bin/browse-edgar'
+            params = {'action': 'getcompany',
+                      'CIK': cik,
+                      'type': '8-K',
+                      'output': 'xml',
+                      'dateb': '20200430',
+                      'datea': '20150501',
+                      'start': '',
+                      'count': '100'}
 
-# In[81]:
+            # getting response from EDGAR database
+            sec_response = requests.get(url=url, params=params)
 
+            # creating soup to parse xml
+            soup = BeautifulSoup(sec_response.content, 'xml')
 
-# getting tweets
+            # getting link to 8-k document
+            urls = soup.findAll('filingHREF')
+            html_list = []
 
-def get_tweets():
-    
-    client = get_twitter_authentication()
+            # html version of links
+            for url in urls:
+                url = url.string
 
-    # connecting to fullarchive endpoint and dev env
+                if url.split('.')[len(url.split('.')) - 1] == 'htm':
+                    txt_link = url + 'l'
+                    html_list.append(txt_link)
 
-    PRODUCT = 'fullarchive'
-    LABEL = 'prod'
-    
-    csv_file = open('D:/Python/stock predictor/tweet.csv', 'w')
-    csv_writer = csv.writer(csv_file)
+            html_list = pd.Series(html_list).astype(str)
 
-    # iterate through all pages and appending username, tweet, and date
+            # list of links
+            doc_list = html_list.str.replace('-index.html', '.txt').values.tolist()
 
-    pager = TwitterPager(client, 'tweets/search/%s/:%s' % (PRODUCT, LABEL), 
-                       {'query':'#stocks OR #markets OR #S&P500',
-                       'fromDate': '201505010000',
-                       'toDate': '202004300000',
-                       'maxResults': 80}
-                      )
+            # creating dataframe to append the link of each company
 
-    for item in pager.get_iterator():
-        if pager.status_code != 200:
-            break
-        
-        # writing to csv row by row 
-        
-        csv_writer.writerow([item['created_at'], 
-                            item['user']['screen_name'], 
-                            item['text'] if 'text' in item else item])
-    
-    return
+            df = pd.DataFrame({'cik': [cik] * len(doc_list),
+                               'txt_link': doc_list})
 
+            doc_df = doc_df.append(df)
+
+        except requests.exceptions.ConnectionError:
+            sleep(.1)
+
+    return doc_df
