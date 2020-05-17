e932a143c5d1efb92524ae4a05ac8464e94056f8
Frank Dong <FrankyDStar@hotmail.com>
2020-05-16 20:51:34 -0400

    added tutorial model and datasets

---
+++ models/LSTM_Stock_Model.py
@@ -0,0 +1,110 @@
+#Script following tutorial by: https://towardsdatascience.com/predicting-stock-price-with-lstm-13af86a74944
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import numpy as np
+from tqdm._tqdm_notebook import tqdm_notebook
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
+from keras import optimizers
+
+
+TIME_STEPS = 60
+BATCH_SIZE = 20
+
+params = {
+    "batch_size": 20,  # 20<16<10, 25 was a bust
+    "epochs": 300,
+    "lr": 0.00010000,
+    "time_steps": 60
+}
+
+#Function that will convert data into timeseries 
+def build_timeseries(mat, y_col_index):
+    # y_col_index is the index of column that would act as output column
+    # total number of time-series samples would be len(mat) - TIME_STEPS
+    dim_0 = mat.shape[0] - TIME_STEPS
+    dim_1 = mat.shape[1]
+    x = np.zeros((dim_0, TIME_STEPS, dim_1))
+    y = np.zeros((dim_0,))
+    
+    for i in tqdm_notebook(range(dim_0)):
+        x[i] = mat[i:TIME_STEPS+i]
+        y[i] = mat[TIME_STEPS+i, y_col_index]
+    print("length of time-series i/o",x.shape,y.shape)
+    return x, y
+
+#will remove odd samples for matching batch sizes
+def trim_dataset(mat, batch_size):
+    """
+    trims dataset to a size that's divisible by BATCH_SIZE
+    """
+    no_of_rows_drop = mat.shape[0]%batch_size
+    if(no_of_rows_drop > 0):
+        return mat[:-no_of_rows_drop]
+    else:
+        return mat
+
+df_ge = pd.read_csv("../data/sample/ge.us.txt", engine='python')
+df_ge.tail()
+
+#Plotting price history
+plt.figure()
+plt.plot(df_ge["Open"])
+plt.plot(df_ge["High"])
+plt.plot(df_ge["Low"])
+plt.plot(df_ge["Close"])
+plt.title('GE stock price history')
+plt.ylabel('Price (USD)')
+plt.xlabel('Days')
+plt.legend(['Open','High','Low','Close'], loc='upper left')
+#plt.show()
+
+#plotting volume
+plt.figure()
+plt.plot(df_ge["Volume"])
+plt.title('GE stock volume history')
+plt.ylabel('Volume')
+plt.xlabel('Days')
+#plt.show()
+
+#Check for numm values
+print("checking if any null values are present\n", df_ge.isna().sum())
+
+#normalizing the data
+train_cols = ["Open","High","Low","Close","Volume"]
+df_train, df_test = train_test_split(df_ge, train_size=0.8, test_size=0.2, shuffle=False)
+print("Train and Test size", len(df_train), len(df_test))
+# scale the feature MinMax, build array
+x = df_train.loc[:,train_cols].values
+min_max_scaler = MinMaxScaler()
+x_train = min_max_scaler.fit_transform(x)
+x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
+
+#create variables for LSTM
+x_t, y_t = build_timeseries(x_train, 3)
+x_t = trim_dataset(x_t, BATCH_SIZE)
+y_t = trim_dataset(y_t, BATCH_SIZE)
+x_temp, y_temp = build_timeseries(x_test, 3)
+x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
+y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
+
+#Run LSTM Model
+lstm_model = Sequential()
+lstm_model.add(LSTM(100, batch_input_shape=(BATCH_SIZE, TIME_STEPS, x_t.shape[2]), dropout=0.0, recurrent_dropout=0.0, stateful=True, kernel_initializer='random_uniform'))
+lstm_model.add(Dropout(0.5))
+lstm_model.add(Dense(20,activation='relu'))
+lstm_model.add(Dense(1,activation='sigmoid'))
+optimizer = optimizers.RMSprop(lr=params["lr"])
+lstm_model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+#Log values and record
+csv_logger = CSVLogger('output' + '.log', append=True)
+
+history = lstm_model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,
+                    shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
+                    trim_dataset(y_val, BATCH_SIZE)), callbacks=[csv_logger])
