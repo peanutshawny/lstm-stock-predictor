d7b6a7fd8c1afdd25b233cf2a8e023e4159f2491
Frank Dong <FrankyDStar@hotmail.com>
2020-09-18 12:45:23 -0700

    added web crawler

--- models/LSTM_Stock_Model (Beginner).py
+++ models/LSTM_Stock_Model (Beginner).py
@@ -39,6 +39,7 @@ in_seq4 = df_ge["Fund_Rate"]
 in_seq5 = df_ge["Neg"]
 in_seq6 = df_ge["Neu"]
 in_seq7 = df_ge["Pos"]
+in_seq8 = df_ge["Unemployement"]
 out_seq = []
 
 for i in range(len(in_seq1)):
@@ -57,12 +58,13 @@ in_seq4 = in_seq4.values.reshape((len(in_seq4), 1))
 in_seq5 = in_seq5.values.reshape((len(in_seq5), 1))
 in_seq6 = in_seq6.values.reshape((len(in_seq6), 1))
 in_seq7 = in_seq7.values.reshape((len(in_seq7), 1))
+in_seq8 = in_seq8.values.reshape((len(in_seq8), 1))
 
 out_seq = out_seq.reshape((len(out_seq), 1))
 
 # horizontally stack columns
 #dataset = hstack((in_seq1, in_seq2, in_seq3, in_seq4, in_seq5, out_seq))
-dataset = hstack((in_seq1, in_seq2, in_seq3, in_seq4, in_seq5, in_seq6, in_seq7, out_seq))
+dataset = hstack((in_seq1, in_seq2, in_seq3, in_seq4, in_seq5, in_seq6, in_seq7, in_seq8, out_seq))
 
 # choose a number of time steps
 n_steps = 30
@@ -94,11 +96,12 @@ t_seq4 = df_td["Fund_Rate"]
 t_seq5 = df_td["Neg"]
 t_seq6 = df_td["Neu"]
 t_seq7 = df_td["Pos"]
+t_seq8 = df_td["Unemployement"]
 t_out = array([t_seq2[i]-t_seq1[i] for i in range(len(t_seq1))])
 
 x_input = []
 for i in range(len(t_seq1)):
-	tempArray = [t_seq1[i], t_seq2[i],t_seq3[i], t_seq4[i], t_seq5[i], t_seq6[i], t_seq7[i], t_out[i]]
+	tempArray = [t_seq1[i], t_seq2[i],t_seq3[i], t_seq4[i], t_seq5[i], t_seq6[i], t_seq7[i], t_seq8[i], t_out[i]]
 	#tempArray = [t_seq1[i], t_seq2[i], t_seq3[i], t_seq4[i], t_seq5[i], t_out[i]]
 	x_input.append(tempArray)
 
---
+++ src/data/f_apiRequest.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getGDP():
+	page = requests.get("https://www.bea.gov/data/gdp/gross-domestic-product")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	gdpValue = soup.find_all(class_='field field--name-field-value field--type-string field--label-hidden field--item')[0].get_text()
+	gdpValue = gdpValue.replace("%", "") 
+	return (float(gdpValue))
+
+def getFund_Rate():
+	page = requests.get("https://www.bankrate.com/rates/interest-rates/federal-funds-rate.aspx")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	fundValue = soup.find_all('tbody')
+	fundValue = str(fundValue[0])
+	fundValue = fundValue.split("<td>")
+	fundValue = fundValue[2].replace("</td>","")
+	return (float(fundValue))
+
+def getUmemployement():
+	page = requests.get("https://fred.stlouisfed.org/series/UNRATE")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	uRate = soup.find_all(class_='series-meta-observation-value')[0].get_text()
+	return (float(uRate))
+
+
+
+print("Current GDP: " + str(getGDP()))
+print("Current Fund Rate: " + str(getFund_Rate()))
+print("Current Unemployement Rate: " + str(getUmemployement()))
